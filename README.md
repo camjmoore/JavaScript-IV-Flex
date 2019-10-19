7b515c21043a560f2df122f3cfd8cc4fc625372c
occludedarp <cmrnmr@gmail.com>
2019-10-18 20:15:52 -0400

    completed lambda-classes

--- README.md
+++ README.md
@@ -68,14 +68,12 @@ const fred = new Instructor({
 ```
 
 #### Person
-
 * First we need a Person class. This will be our `base-class`
 * Person receives `name` `age` `location` all as props
 * Person receives `speak` as a method.
 * This method logs out a phrase `Hello my name is Fred, I am from Bedrock` where `name` and `location` are the object's own props
 
 #### Instructor
-
 * Now that we have a Person as our base class, we'll build our Instructor class.
 * Instructor uses the same attributes that have been set up by Person
 * Instructor has the following unique props:
@@ -87,7 +85,6 @@ const fred = new Instructor({
   * `grade` receives a `student` object and a `subject` string as arguments and logs out '{student.name} receives a perfect score on {subject}'
 
 #### Student
-
 * Now we need some students!
 * Student uses the same attributes that have been set up by Person
 * Student has the following unique props:
@@ -100,7 +97,6 @@ const fred = new Instructor({
   * `sprintChallenge` similar to PRAssignment but logs out `student.name has begun sprint challenge on {subject}`
 
 #### Project Manager
-
 * Now that we have instructors and students, we'd be nowhere without our PM's
 * ProjectManagers are extensions of Instructors
 * ProjectManagers have the following unique props:
@@ -111,7 +107,6 @@ const fred = new Instructor({
   * `debugsCode` a method that takes in a student object and a subject and logs out `{name} debugs {student.name}'s code on {subject}`
 
 #### Stretch Problem
-
 * Extend the functionality of the Student by adding a prop called grade and setting it equal to a number between 1-100.
 * Now that our students have a grade build out a method on the Instructor (this will be used by _BOTH_ instructors and PM's) that will randomly add or subtract points to a student's grade. _Math.random_ will help.
 * Add a graduate method to a student.
--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,118 @@
-// CODE here for your Lambda Classes
+class Person {
+  constructor(attributes){
+    this.name = attributes.name;
+    this.age = attributes.age;
+    this.location = attributes.location;
+  };
+    speak(){
+        console.log(`Hello my name is ${this.name}, I am from ${this.location}`)
+    };
+};
+
+class Instructor extends Person {
+  constructor(attributes){
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favLanguage = attributes.favLanguage;
+    this.catchPhrase = attributes.catchPhrase;
+  };
+    demo(subject){
+      console.log(`Today we are learning about ${subject}`)
+    };
+    grade(student, subject){
+      console.log(`${student} receives a perfect score on ${subject}`)
+    };
+};
+
+class Student extends Person {
+  constructor(attributes){
+    super(attributes);
+    this.previousBackground = attributes.previousBackground;
+    this.className = attributes.className;
+    this.favSubjects = attributes.favSubjects;
+  };
+    listsSubjects(){
+      console.log(this.favSubjects)
+    };
+    PRAssignment(subject){
+      console.log(`${student.name} has submitted a PR for ${subject}`)
+    };
+    sprintChallenge(subject){
+      console.log(`${student.name} has begun a sprint challenge on ${subject}`)
+    };
+};
+
+class ProjectManager extends Instructor {
+  constructor(attributes){
+    super(attributes)
+    this.gradClassName = attributes.gradClassName;
+    this.favInstructors = attributes.favInstructors;
+  };
+    standUp(name, channel){
+      console.log(`${name} announces to ${channel}, @channel standy times!`)
+    };
+    debugsCode(student, subject){
+      console.log(`${this.name} debugs ${student.name}'s code on ${subject}`)
+    };
+};
+
+const kingCold = new Instructor({
+  name: 'kingCold',
+  location: 'Hell',
+  age: 790,
+  favLanguage: 'Java',
+  specialty: 'Android Development',
+  catchPhrase: 'My clan is feared throughout the Universe! What fool dares challenge our supremacy?'
+});
+
+const korin = new Instructor({
+  name: 'Korin',
+  location: 'Korin Tower',
+  age: 800,
+  favLanguage: 'C++',
+  specialty: 'climbing all levels of the stack',
+  catchPhrase: 'The cat has a name you know! And it wouldn\'t hurt for you to show a little respect!'
+});
+
+
+const trunks = new Student({
+  name: 'Trunks',
+  location: 'Earth',
+  age: 7,
+  previousBackground: 'baby',
+  className: 'FullStack',   
+  favSubjects: 'functional programming'
+})
+
+const gohan = new Student({
+  name: 'Gohan',
+  location: 'Earth',
+  age: 29,
+  previousBackground: 'Z Fighter',
+  className: 'DataScience',   
+  favSubjects: 'data analyis'
+})
+
+
+const piccolo = new ProjectManager({
+  name: 'Piccolo',
+  location: 'Namek',
+  age: 120,
+  favLanguage: 'python',
+  specialty: 'Dev Ops',
+  catchPhrase: 'After all my years of training to defeat your father, I go out like this trying to save you; his son.',
+  gradClassName: 'iOS3',
+  favInstructors: 'korin'
+})
+
+const frieza = new ProjectManager({
+  name: 'Frieza',
+  location: 'universe 7',
+  age: 36,
+  favLanguage: 'namekian',
+  specialty: 'Death Comet',
+  catchPhrase: 'There are three things I refuse to tolerate. Cowardice. Bad haircuts, and military insurrection.',
+  gradClassName: 'FullStack7',
+  favInstructors: 'King Cold'
+})
+
